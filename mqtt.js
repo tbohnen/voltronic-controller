16feedc7167829096774a0e3de8a5ddaaa64018e
root <tbohnen.jnr@gmail.com>
2020-05-19 10:15:55 +0000

    Update mqtt to register sensors on startup

--- mqtt.js
+++ mqtt.js
@@ -14,14 +14,95 @@ const publishStatus = async () => {
 
 	for (const key of Object.keys(status)){
 		const value = status[key];
-		console.log(`publishing ${key}:${value}`);
-		client.publish(`homeassistant/sensor/voltronic_${key}`, value.toString());
+		console.log(`publishing (homeassistant/sensor/voltronic_${key}) ${key}:${value}`);
+		client.publish(`homeassistant/sensor/voltronic_${key}`, value.toString(), [], (e) => console.log(e));
 	}
 }
+
+const registerTopic = (key, unitOfMeasurement, icon) => {
+	const mqttDeviceName = "voltronic"
+	const topic = "homeassistant"
+	
+	const message = { 
+		"name": `${mqttDeviceName}_${key}`,
+		"unit_of_measurement": unitOfMeasurement,
+		"state_topic": `${topic}/sensor/${mqttDeviceName}_${key}`,
+		"icon": `mdi:${icon}`
+
+
+	}
+	console.log(`registering: ${topic}/sensor/${mqttDeviceName}_${key}/config: ${JSON.stringify(message)}`);
+
+	client.publish(`${topic}/sensor/${mqttDeviceName}_${key}/config`, JSON.stringify(message), [], (e) => console.log(e));
+}
+
+
+//registerTopic () {
+    //mosquitto_pub \
+        //-h $MQTT_SERVER \
+        //-p $MQTT_PORT \
+        //-u "$MQTT_USERNAME" \
+        //-P "$MQTT_PASSWORD" \
+        //-t "$MQTT_TOPIC/sensor/"$MQTT_DEVICENAME"_$1/config" \
+        //-m "{
+            //\"name\": \""$MQTT_DEVICENAME"_$1\",
+            //\"unit_of_measurement\": \"$2\",
+            //\"state_topic\": \"$MQTT_TOPIC/sensor/"$MQTT_DEVICENAME"_$1\",
+            //\"icon\": \"mdi:$3\"
+        //}"
+//}
+
+//registerInverterRawCMD () {
+    //mosquitto_pub \
+        //-h $MQTT_SERVER \
+        //-p $MQTT_PORT \
+        //-u "$MQTT_USERNAME" \
+        //-P "$MQTT_PASSWORD" \
+        //-t "$MQTT_TOPIC/sensor/$MQTT_DEVICENAME/config" \
+        //-m "{
+            //\"name\": \""$MQTT_DEVICENAME"\",
+            //\"state_topic\": \"$MQTT_TOPIC/sensor/$MQTT_DEVICENAME\"
+        //}"
+//}
 
 
 
 let client
+
+const registerTopics = () => {
+	registerTopic("Inverter_mode","","solar-power")//# 1 = Power_On, 2 = Standby, 3 = Line, 4 = Battery, 5 = Fault, 6 = Power_Saving, 7 = Unknown
+	registerTopic("AC_grid_voltage","V","power-plug")
+	registerTopic("AC_grid_frequency","Hz","current-ac")
+	registerTopic("AC_out_voltage","V","power-plug")
+	registerTopic("AC_out_frequency","Hz","current-ac")
+	registerTopic("PV_in_voltage","V","solar-panel-large")
+	registerTopic("PV_in_current","A","solar-panel-large")
+	registerTopic("PV_in_watts","W","solar-panel-large")
+	registerTopic("PV_in_watthour","Wh","solar-panel-large")
+	registerTopic("SCC_voltage","V","current-dc")
+	registerTopic("Load_pct","%","brightness-percent")
+	registerTopic("Load_watt","W","chart-bell-curve")
+	registerTopic("Load_watthour","Wh","chart-bell-curve")
+	registerTopic("Load_va","VA","chart-bell-curve")
+	registerTopic("Bus_voltage","V","details")
+	registerTopic("Heatsink_temperature","","details")
+	registerTopic("Battery_capacity","%","battery-outline")
+	registerTopic("Battery_voltage","V","battery-outline")
+	registerTopic("Battery_charge_current","A","current-dc")
+	registerTopic("Battery_discharge_current","A","current-dc")
+	registerTopic("Load_status_on","","power")
+	registerTopic("SCC_charge_on","","power")
+	registerTopic("AC_charge_on","","power")
+	registerTopic("Battery_recharge_voltage","V","current-dc")
+	registerTopic("Battery_under_voltage","V","current-dc")
+	registerTopic("Battery_bulk_voltage","V","current-dc")
+	registerTopic("Battery_float_voltage","V","current-dc")
+	registerTopic("Max_grid_charge_current","A","current-ac")
+	registerTopic("Max_charge_current","A","current-ac")
+	registerTopic("Out_source_priority","","grid")
+	registerTopic("Charger_source_priority","","solar-power")
+	registerTopic("Battery_redischarge_voltage","V","battery-negative")
+}
 
 const init = () => {
 	setInterval(publishStatus, options.publishStatusSeconds*1000);
@@ -35,6 +116,7 @@ const init = () => {
 
 	client.on('connect', function (e) {
 		publishStatus()
+		registerTopics()
 		  client.subscribe(options.commandsTopic, function (err) {
 			      if (err) { console.log('could not subscribe', err); }
 			  else { 
@@ -76,9 +158,6 @@ module.exports = {
 	publish,
 	init
 }
-
-
-
 
 
 //Inverter_mode=`echo $INVERTER_DATA | jq '.Inverter_mode' -r`
