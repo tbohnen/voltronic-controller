3db3cdb6b463eddfa48051cae25a3bcd90a7cda8
theo <tbohnen.jnr@gmail.com>
2023-02-08 05:33:10 +0000

    Move inverter poller to bridge folder, so that it can run on separate machine

---
+++ bridge/axpert-mqtt-bridge.js
@@ -0,0 +1,112 @@
+const { exec } = require('child_process');
+const { EventEmitter } = require("events");
+const emitter = new EventEmitter();
+const options = require('./options')
+const mqtt = require('./bridge-mqtt');
+
+const executeRaw = (cmd) => {
+	console.log('executing', cmd);
+	return new Promise((resolve, reject) => { 
+		
+		exec(`./inverter_poller -r ${cmd}`, (err, stdout, stderr) => {
+		  if (err) {
+			console.error(err);
+			reject(err);
+		    // node couldn't execute the command
+		    return;
+		  }
+
+		resolve(stdout);
+		if (stderr) {
+			console.log('error', stderr);
+			reject(stderr);
+		}
+		});
+	});
+}
+
+let status = null
+
+const getStatus = (cached = true) => {
+	return new Promise((resolve, reject) => { 
+		if (cached && status) {
+			resolve(status)
+			return
+		}
+
+		exec("sudo sh -c './inverter_poller -1'", (err, stdout, stderr) => {
+		  if (err) {
+			console.error(err);
+			reject(err);
+		    // node couldn't execute the command
+		    return;
+		  }
+                  if (stderr) {
+			  reject(stderr);
+			  console.error(stderr);
+			  return;
+		  }
+		  const date = new Date();
+		  status = { type: "axpert", time: date.toISOString(), ...JSON.parse(stdout) }
+		  console.log('status', status);
+	          resolve(status);
+		});
+	});
+}
+
+const publishStatus = async () => {
+	console.log('getting status')
+	const status = await getStatus(false)
+	console.log('status', status)
+	mqtt.publish("/solar", JSON.stringify(status));
+}
+
+
+
+const toggleSource = async (source) => {
+	let code = "";
+
+	if (source === "solar"){
+		code = "02";
+	} else if (source === "utility") {
+		code = "00";
+	}
+
+	console.log('switching source to', source)
+
+  const outcome2 = await executeRaw(`POP${code}`);
+  console.log('POP outcome', outcome2)
+  await sleepMs(2000);
+  const outcome1 = await executeRaw(`PCP${code}`);
+  console.log('PCP outcome', outcome1)
+}
+
+const execute = async (command, value) => {
+	console.log(`executing command: ${command}, value: ${value}`);
+	switch (command) {
+		case "source": {
+			await toggleSource(value)
+     break;
+    }
+    case "mqtt": {
+      await publishMqttCommand(value)
+    }
+	}
+}
+
+const sleepMs = (ms) => {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => resolve(), ms);
+	});
+}
+
+mqtt.init()
+setInterval(publishStatus, options.publishStatusSeconds * 1000);
+publishStatus()
+
+module.exports = {
+	execute,
+	executeRaw,
+	getStatus,
+	emitter
+}
